4c5ca8cc6a55bbea81d21a8590de0f60bf416bc1
owenhai <hledell150506@gmail.com>
2024-10-08 10:33:36 +0700

    123132

---
+++ TetsMain2.java
@@ -0,0 +1,39 @@
+<<<<<<< HEAD
+public class TetsMain2
+{
+    public static void main(String[] args)
+    {
+    Employee e1 = new Employee(8,"Duc","Hai",3000);
+    System.out.println(e1);
+    e1.setSalary(5000);
+    System.out.println(e1);
+    System.out.println("id is:" + e1.getId());
+    System.out.println("firstname is:" + e1.getFirstname());
+    System.out.println("lastname is:" + e1.getLastname());
+    System.out.println("salary is:" + e1.getSalary());
+    System.out.println("name is:" + e1.getName());
+    System.out.println("anual salary is:" + e1.getAnnualSalary());
+    System.out.println(e1.raiseSalary(50));
+    System.out.println(e1);
+    }
+}
+=======
+public class TetsMain2
+{
+    public static void main(String[] args)
+    {
+    Employee e1 = new Employee(8,"Duc","Hai",3000);
+    System.out.println(e1);
+    e1.setSalary(5000);
+    System.out.println(e1);
+    System.out.println("id is:" + e1.getId());
+    System.out.println("firstname is:" + e1.getFirstname());
+    System.out.println("lastname is:" + e1.getLastname());
+    System.out.println("salary is:" + e1.getSalary());
+    System.out.println("name is:" + e1.getName());
+    System.out.println("anual salary is:" + e1.getAnnualSalary());
+    System.out.println(e1.raiseSalary(50));
+    System.out.println(e1);
+    }
+}
+>>>>>>> 735151a589353871c9dbdae0afa4cc0c0fbaf6f2
